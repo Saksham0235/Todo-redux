81aac99dc04b1c632e2df39ad1c594b128bd3321
Saksham <ssaksham952@gmail.com>
2024-07-06 16:18:03 +0530

    Added Checkbox

--- src/Store/Todoslice.jsx
+++ src/Store/Todoslice.jsx
@@ -1,18 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
-const initialState={
-    tasks:[]
+const initialState = {
+    tasks: []
 }
 
-const todosSlice=createSlice({
-    name:'tasks',
+const todosSlice = createSlice({
+    name: 'tasks',
     initialState,
-    reducers:{
-        addTask:(state,action)=>{
-         state.tasks.push({...action.payload,id:uuidv4()});
+    reducers: {
+        addTask: (state, action) => {
+            state.tasks.push({ ...action.payload, id: uuidv4() });
         },
-        editTask:(state,action)=>{
+        editTask: (state, action) => {
             const { id, title, description } = action.payload;
             const existingTask = state.tasks.find(task => task.id === id);
             if (existingTask) {
@@ -20,17 +20,24 @@ const todosSlice=createSlice({
                 existingTask.description = description;
             }
         },
-        deleteTask:(state,action)=>{
+        deleteTask: (state, action) => {
             return {
                 ...state,
                 tasks: state.tasks.filter((task) => task.id !== action.payload)
             }
+        },
+        checkboxTaskTodo: (state, action) => {
+            const existingTask = state.tasks.find(task => task.id === action.payload);
+            if (existingTask) {
+                existingTask.completed = !existingTask.completed;
+            }
+
         }
-        
+
     }
 })
 
 
-export const { addTask,deleteTask,editTask} =todosSlice.actions;
+export const { addTask, deleteTask, editTask, checkboxTaskTodo } = todosSlice.actions;
 export default todosSlice.reducer
 
--- src/Todos/Tasks.jsx
+++ src/Todos/Tasks.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { deleteTask, editTask } from '../Store/Todoslice'
+import { deleteTask, editTask, checkboxTaskTodo } from '../Store/Todoslice'
 import TaskForm from './Todosform'
 import EditForm from './EditForm'
-import {  DeleteOutlined } from '@ant-design/icons'
+import { DeleteOutlined } from '@ant-design/icons'
+import { Checkbox } from 'antd'
+import { useSnackbar } from 'notistack';
 
 const Tasks = () => {
+
+    const { enqueueSnackbar } = useSnackbar();
+
     const dispatch = useDispatch();
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("");
     const todos = useSelector((state) => state.tasks.tasks);
     const [selectedTask, setSelectedTask] = useState(null);
     const [selectedTaskID, setSelectedTaskID] = useState(null);
+    const [completed, setCompleted] = useState(false)
     const [isEdit, setIsEdit] = useState(false)
 
     const handleEdit = (id) => {
         const data = todos.find((task) => task.id === id);
-        console.log(data,"From selectedtask");
+        console.log(data, "From selectedtask");
         setSelectedTask(data);
         setSelectedTaskID(id);
         setIsEdit(true)
@@ -27,6 +31,17 @@ const Tasks = () => {
     }
     const toggleForm = () => {
         setSelectedTaskID(null);
+    }
+    const handleChecbox = async (taskId) => {
+        setCompleted(true)
+        try {
+            console.log("Before updating", completed);
+            dispatch(checkboxTaskTodo(taskId))
+            console.log(completed, "State of complete in checkbox");
+            enqueueSnackbar(<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: 200 }}><span style={{ fontSize: '15px', fontWeight: 550 }}>1 task completed </span></div>, { variant: 'info' })
+        } catch (error) {
+            console.log('Error in checkboxTask', error);
+        }
     }
     return (
         <>
@@ -42,12 +57,23 @@ const Tasks = () => {
                                         <ul className='taskdiv'>
                                             <li className='task' style={{ listStyle: 'none', display: 'flex', justifyContent: 'space-between' }}>
                                                 <div className='taskdisplay'>
-                                                    <span style={{ fontSize: '20px', fontWeight: '550' }}>{task.title}</span>
-                                                    <span style={{ fontSize: '13px', color: 'gray', marginTop: '2px' }}>{task.description}</span>
+                                                    <div style={{width:'25rem'}}>
+                                                        <Checkbox
+                                                            style={{
+                                                                paddingRight: '1rem', marginTop: '-5px'
+                                                            }}
+                                                            onChange={() => {
+                                                                handleChecbox(task.id)
+                                                            }}
+                                                        ></Checkbox>
+                                                        <span style={{ fontSize: '20px', fontWeight: '550' }}>{task.title}</span>
+                                                        <span style={{marginLeft:'3rem'}}>Status: {task.completed ? 'Completed' : "InComplete"}</span>
+                                                    </div>
+                                                    <span style={{ fontSize: '13px',width:'10rem', color: 'gray', marginTop: '2px',marginLeft:'2rem' }}>{task.description}</span>
                                                 </div>
                                                 <div className='taskbtns'>
                                                     <button className='btn1' onClick={() => handleEdit(task.id)} >Edit</button>
-                                                    <button className='btn2' onClick={() => handleDelete(task.id)} ><DeleteOutlined  twoToneColor= "red" /></button>
+                                                    <button className='btn2' onClick={() => handleDelete(task.id)} ><DeleteOutlined twoToneColor="red" /></button>
                                                 </div>
                                             </li>
                                         </ul>
